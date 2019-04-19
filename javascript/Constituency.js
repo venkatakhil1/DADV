dd659ddbaa17ef5f5815d147528fe4bca16bae70
akhil <venkatakhil123@gmail.com>
2019-04-19 10:41:54 +0530

    updation 2
    changing constituency wise

---
+++ javascript/Constituency.js
@@ -0,0 +1,25 @@
+function sub() {
+    // body...
+    var state=document.getElementById("S1").value;
+    var Constituency=document.getElementById("S2").value;
+    //alert(state+" "+Constituency);
+
+    var a = state+Constituency;
+
+    $.ajax({
+  				type:"GET",
+  				url:"data1/"+a+".csv",
+  				dataType:"text",
+  				success: function(data) {
+  					processData(data);
+  				}
+
+  			});
+
+    document.getElementById("form").submit();
+    
+}
+function processData(data){
+	alert(data);
+
+}
